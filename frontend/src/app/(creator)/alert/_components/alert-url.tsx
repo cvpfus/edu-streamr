bc3a85a592b31b662631acf1f10a2d65f43e5b67
cvpfus <47532266+yusufpraditya@users.noreply.github.com>
2025-03-09 22:20:41 +0700

    fix: fix dragging issue in firefox

--- frontend/src/app/(creator)/alert/_components/alert-url.tsx
+++ frontend/src/app/(creator)/alert/_components/alert-url.tsx
@@ -1,12 +1,14 @@
 import {
   Card,
-  CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import CopyButton from "@/components/ui/copy-button";
+import React from "react";
+import Link from "next/link";
 
 export const AlertUrl = ({
   fullUrl,
@@ -15,20 +17,58 @@ export const AlertUrl = ({
   fullUrl: string;
   username?: string;
 }) => {
+  const obsUrl = `${fullUrl}?layer-name=EduStreamr Alert&layer-width=600&layer-height=200`;
+
+  const handleDragStart = (e: React.DragEvent<HTMLAnchorElement>) => {
+    const exampleAlert = document.getElementById("example-alert");
+
+    if (exampleAlert) {
+      e.dataTransfer.setDragImage(exampleAlert, 0, 0);
+    } else {
+      const img = document.createElement("img");
+      img.setAttribute("src", "/logo.png");
+      img.setAttribute("alt", "EduStreamr");
+      img.id = "edustreamr-logo";
+      img.hidden = true;
+      document.body.appendChild(img);
+      e.dataTransfer.setDragImage(img, 0, 0);
+    }
+
+    e.dataTransfer.setData("text/uri-list", obsUrl);
+  };
+
+  const handleDragEnd = () => {
+    const img = document.getElementById("edustreamr-logo");
+
+    if (img) {
+      document.body.removeChild(img);
+    }
+  };
+
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>Alert URL</CardTitle>
-        <CardDescription>
-          Copy this URL and paste it to the OBS Browser Source.
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <div className="flex flex-col items-start gap-4">
-          <Input value={fullUrl} readOnly />
-          <CopyButton text={fullUrl} disabled={!username} />
-        </div>
-      </CardContent>
-    </Card>
+    <Link
+      href={obsUrl}
+      onClick={(e) => e.preventDefault()}
+      className="cursor-default w-full"
+      onDragStartCapture={handleDragStart}
+      onDragEndCapture={handleDragEnd}
+      draggable
+    >
+      <Card className="w-full flex flex-col">
+        <CardHeader>
+          <CardTitle>Alert URL</CardTitle>
+          <CardDescription>
+            Copy this URL and paste it into the OBS Browser Source. Or, simply
+            drag and drop this box into OBS to create the source instantly.
+          </CardDescription>
+        </CardHeader>
+        <CardFooter className="flex-grow flex items-end">
+          <div className="flex gap-4 w-full">
+            <Input value={fullUrl} readOnly />
+            <CopyButton text={fullUrl} disabled={!username} />
+          </div>
+        </CardFooter>
+      </Card>
+    </Link>
   );
 };
