34a3b262df9984dce8c6abd9f741bfdc5e81fe37
cvpfus <47532266+yusufpraditya@users.noreply.github.com>
2025-03-09 22:28:49 +0700

    refactor: layout refactor

--- frontend/src/app/(creator)/layout.tsx
+++ frontend/src/app/(creator)/layout.tsx
@@ -1,6 +1,8 @@
 import { ConnectButton } from "@/components/ui/connect-button";
 import SideNav from "./_components/side-nav";
 import Connect from "./connect";
+import { MobileSideNav } from "./_components/mobile-side-nav";
+import { RefreshButton } from "@/components/ui/refresh-button";
 
 export default async function Layout({
   children,
@@ -10,9 +12,13 @@ export default async function Layout({
   return (
     <div className="flex">
       <SideNav />
-      <div className="flex flex-col w-full px-4 h-dvh max-h-dvh max-w-[calc(100dvw-64px)]">
-        <div className="flex justify-end mt-4 ">
-          <ConnectButton />
+      <div className="flex flex-col w-full sm:max-w-[calc(100%-225px)] px-4 h-dvh max-h-dvh">
+        <div className="flex gap-4 justify-between items-center mt-4 w-full">
+          <MobileSideNav />
+          <div className="w-full flex gap-4 justify-between items-center">
+            <RefreshButton />
+            <ConnectButton />
+          </div>
         </div>
         <div className="h-full w-full mt-4">
           <Connect>{children}</Connect>
